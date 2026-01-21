d329c4a6664e69f7dec18acc4a0b309cea49038c
OussemaBouchaala <145915771+OussemaBouchaala@users.noreply.github.com>
2026-01-21 17:38:49 +0100

    oussema add recruiter,candidate and jobs fetch

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,10 +3,29 @@ import { API_URL } from '../config/api/api_url';
 import { HttpService } from '@nestjs/axios';
 import { Observable, map } from 'rxjs';
 import FormData from 'form-data';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Recruiter } from './entities/recruiter.entity';
+import { Repository } from 'typeorm';
+import { Candidate } from './entities/candidate.entity';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    @InjectRepository(Recruiter)
+    private recruiterRepository: Repository<Recruiter>,
+    @InjectRepository(Candidate)
+    private candidateRepository: Repository<Candidate>,
+  ) {}
+
+  findAllRecruiters(): Promise<Recruiter[]> {
+    return this.recruiterRepository.find();
+  }
+
+  findAllCandidates(): Promise<Candidate[]> {
+    return this.candidateRepository.find();
+  }
+
   testing_api():Observable<JSON> {
     return this.httpService.get(API_URL.testingAPIURL).pipe(map(res => res.data));
   
